6c3eabac4bc45e39083ea1cef5561827ad74d864
Jhan Gale Garcia <garcia.johngale@gmail.com>
2023-09-03 12:04:33 +0800

    fix: error route aligning problem

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
 	return (
 		<div className="flex flex-col justify-center items-center h-[100vh]">
 			<h1 className="text-[6em] m-0">404</h1>
-			<p className="text-[1.5em] m-0 mt-[20px] mb-5">You&apos;ve lost track! page not found</p>
+			<p className="text-[1.5em] text-center m-0 mt-[20px] mb-5">You&apos;ve lost track! page not found</p>
 			<Link className={buttonVariants({ variant: "default" })} href="/">
 				Take me back
 			</Link>
